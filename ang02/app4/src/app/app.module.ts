e0a656ed72ef727b21e6c296d20e8e059bad2ae5
Maximiliano Usich <maximiliano.usich@softvision.com>
2020-07-20 14:23:08 -0300

    angular ngModel

--- ang02/app4/src/app/app.module.ts
+++ ang02/app4/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SecondComponent } from './second/second.component';
 
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,7 +13,8 @@ import { SecondComponent } from './second/second.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
