c6a5f677331223c768cd73c0673203c7a3b02bd0
Maximiliano Usich <maximiliano.usich@softvision.com>
2020-07-21 15:59:34 -0300

    angular, api rest pagination

--- ang03/app30/server/generate.js
+++ ang03/app30/server/generate.js
@@ -7,6 +7,7 @@ for (let i = 1; i <= 50; i++) {
   database.products.push({
     id: i,
     name: faker.commerce.productName(),
+    material: faker.commerce.productMaterial(),
     price: faker.commerce.price(),
     imageUrl: 'https://source.unsplash.com/1600x900/?product',
     quantity: faker.random.number(),
--- ang03/app30/src/app/data.service.ts
+++ ang03/app30/src/app/data.service.ts
@@ -1,18 +1,70 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpParams } from '@angular/common/http';
+
+import { throwError } from 'rxjs';
+import { retry, catchError, tap } from 'rxjs/operators';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  private REST_API_SERVER = "http://localhost:3000";
+  private REST_API_SERVER = "http://localhost:3000/products";
+
+  public first:  string = "";
+  public prev:   string = "";
+  public next:   string = "";
+  public last:   string = "";
 
   constructor(private httpClient: HttpClient) { 
 
   }
 
-  public sendGetRequest(){
-    return this.httpClient.get( `${this.REST_API_SERVER}/products` );
+  handleError(error: HttpErrorResponse){
+    let errorMessage = "Unknown error!";
+    if(error.error instanceof ErrorEvent){
+      errorMessage = `Error: ${error.error.message}`;
+    }else{
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    window.alert(errorMessage);
+    return throwError(errorMessage);
   }
+
+  public sendGetRequest(){
+    //const options = { params: new HttpParams({fromString: "_page=1&_limit=5"}) };
+    return this.httpClient.get( this.REST_API_SERVER, 
+      { params: new HttpParams( { fromString: "_page=1&_limit=5"} ), 
+          observe: "response" } )
+            .pipe( retry(3), catchError( this.handleError ), tap( res => {
+              console.log(res.headers.get('Link'));
+              this.parseLinkHeader(res.headers.get('Link'))
+            }) );
+  }
+  public sendGetRequestToUrl(url: string){
+    return this.httpClient.get(url, { observe: "response"})
+    .pipe(retry(3), catchError(this.handleError), tap(res => {
+      console.log(res.headers.get('Link'));
+      this.parseLinkHeader( res.headers.get('Link') )
+    }));
+  }
+  parseLinkHeader(header){
+    if(header && header.length === 0){
+      return;
+    }
+    let parts = header.split(',');
+    const links = {};
+    parts.forEach( p => {
+      let section = p.split(';');
+      const url = section[0].replace(/<(.*)>/,'$1').trim();
+      const name = section[1].replace(/rel="(.*)"/, '$1').trim();
+      links[name] = url;
+    });
+    this.first = links["first"];
+    this.prev = links["prev"];
+    this.next = links["next"];
+    this.last = links["last"];
+  }
+
 }
